a370d277ec3d09c311e4db7d0fb3c5ceb46ef0bf
PhysRex <ThePhysRex@gmail.com>
2017-10-26 19:03:59 -0400

    finalized site files

--- script.js
+++ script.js
@@ -1,99 +1,60 @@
-(function () {
-
-})();
-
-
-
-
-
-
-
-
-
 
 var handler = {
 	listeners: function() {
     // Event listeners for buttons clicked
     
-		// window.addEventListener( "click", this.btnPress );
-		["click", "keydown", "touchend"].forEach( function(val) {
-			window.addEventListener( val, this.btnPress );
-		}, this);
+		window.addEventListener( "click", this.btnPress );
+		// ["click", "keydown", "touchend"].forEach( function(val) {
+		// 	window.addEventListener( val, this.btnPress );
+		// }, this);
 	},
-	btnPress: function() {
-		// get element that was clicked as an event		
-
+	btnPress: function(event) {
+		// get element that was clicked as an event
     
     var elementClicked = event.target;
+    // console.log(elementClicked);
     
+    function classTest(text) {
+      // return true if text is in classList
+      var test = elementClicked.classList.contains(text);
+      return test;
+    } 
 
-    console.log(elementClicked)
-
-		function tagClass(text) {
+		function idTest(text) {
 			// return true if text is in classList
-			var test = elementClicked.classList.contains(text);
+			var test = elementClicked.id === text;
 			return test;
-		} 
-		var keyCode = event.keyCode;
-		// set btnID as ID of the button clicked
-		var id =  (elementClicked.id===undefined) ? 0 : elementClicked.id;	
-		var className = (elementClicked.className===undefined) ? 0 : elementClicked.className ;
-		var type = (elementClicked.type===undefined) ? 0 : elementClicked.type ;
-		var checked = (elementClicked.checked===undefined) ? 0 : elementClicked.checked ;
-		
-		// console.log(...)
-		// console.log(event);
-		// console.log("element:",
-		// 						"\n      id:", id, 
-		// 						"\n   class:", className,
-		// 						"\n    type:", type,
-		// 						"\n checked:", checked);
+    } 
     
     var newsTitleElem = document.getElementById("aside-title-news");
     var newsPanelElem = document.getElementById("news");
     var archiveTitleElem = document.getElementById("aside-title-archive");
     var archivePanelElem = document.getElementById("archive");
-		if (tagClass("aside-panel-header")){
-      
-      newsPanelElem.classList.toggle("hide");      
-      archivePanelElem.classList.toggle("hide");
+		if (idTest("aside-title-news")){
+      newsPanelElem.classList.remove("hide");      
+      archivePanelElem.classList.add("hide");
 
-      newsTitleElem.classList.toggle("active");
-      archiveTitleElem.classList.toggle("active");
-		}
-				
-		// if (tagClass("strict")){
-		// 		if (checked) {
-		// 			simon.strictMode = 1;
-		// 		} else {
-		// 			simon.strictMode = 0;
-		// 		}
-		// }
-		
-		// if (tagClass("tile") && simon.simon === 1){
-		// 	// check if correct
-		// 	simon.check(event);			
-		// }
-    
-    
+      newsTitleElem.classList.add("active");
+      archiveTitleElem.classList.remove("active");
+		} else if (idTest("aside-title-archive")) {
+      newsPanelElem.classList.add("hide");      
+      archivePanelElem.classList.remove("hide");
 
+      newsTitleElem.classList.remove("active");
+      archiveTitleElem.classList.add("active");
+    }
     
 
     // Get the modal
     var modal = document.getElementById('login-modal');
 
-    // Get the button that opens the modal
-    var openBtn = document.getElementById("aside-login");
-    
-    // Get the <span> element that closes the modal
-    var closeBtn = document.getElementById("modal-close");
-
     // When the user clicks on the button, open the modal 
-    if (tagClass("aside-button")) {
+    if (classTest("aside-button")) {
       modal.style.display = "block";
-    } 
+    }
+
     // When the user clicks on <span> (x) or outside modal, close the modal
-    else if (tagClass("modal-close") || tagClass("modal") || tagClass("modal-close-img")) {
+    else if (classTest("modal-close") || classTest("modal") || classTest("modal-close-img")) {
       modal.style.display = "none";
     }
     
@@ -104,57 +65,101 @@ var handler = {
 	}
 }
 
-var storeLocally = {
-  testLocalStorage: function() {
-    if (typeof(Storage) === "undefined") {
-      alert("Sorry! No Web Storage support");
-      return;
-    }
-  },
-  keepValues: function(arrToRetrieve) {
-    this.testLocalStorage();
-    localStorage.setItem(arrToRetrieve, JSON.stringify(bookmarks.bList));
-  },
-  retrieveValues: function(arrToRetrieve) {
-    this.testLocalStorage();
-    if (localStorage.getItem(arrToRetrieve)) {
-      bookmarks.bList = JSON.parse(localStorage.getItem(arrToRetrieve));
-    } else {
-      bookmarks.bList = [];
-    }
-    
-  },
-  localStorageSpace: function() {
-    // output local storage used
-        var allStrings = '';
-        for(var key in window.localStorage){
-            if(window.localStorage.hasOwnProperty(key)){
-                allStrings += window.localStorage[key];
-            }
-        }
-        
-        return allStrings ? 3 + Math.round(1000*(allStrings.length*16)/(8*1024))/1000 + ' KB' + '/' + this.localStorageSpaceTotal() + 'KB' + ' (' + Math.round(10000000*((allStrings.length*16)/(8*1024))/this.localStorageSpaceTotal())/10000000 + '%)'  : 'Empty (0 KB' +  + '/' + this.localStorageSpaceTotal() + 'KB )';
-  },
-  localStorageSpaceTotal: function() {
-    function gen(n) {
-      return new Array((n * 1024) + 1).join('a')
-    }
+var actions = {
+  navBtn: function (elem) {
+    elem.classList.toggle("change");
 
-    // localStorage.removeItem('size');
-    // Determine size of localStorage if it's not set
-    if (localStorage && !localStorage.getItem('size')) {
-      var i = 0;
-      try {
-          // Test up to 50 MB
-          for (i = 250; i <= 10000; i += 250) {
-              localStorage.setItem('test', new Array((i * 1024) + 1).join('a'));
-          }
-      } catch (e) {
-          localStorage.removeItem('test');
-          localStorage.setItem('size', i - 250);            
+    const navList = document.querySelector(".nav-list");
+    // navList.style.display = "block";
+    navList.classList.toggle("show");
+  },
+  makeTable: function (dataObj) {
+    // console.log(dataObj);
+
+    // get table body
+    const tableElem = document.querySelector("#table-body");
+
+    // delete table body (children)
+    tableElem.innerText = "";
+
+    // sort data by earnings
+    dataObj.sort((a,b) => {
+      return b.earnings - a.earnings;
+    });
+
+    // make base elements
+    dataObj.forEach((val, index) => {
+      const tr = document.createElement("tr");
+      const tdName = document.createElement("td");
+      const tdRate = document.createElement("td");
+      const tdEarn = document.createElement("td");
+
+      // format data
+      tdName.innerText = val.name;
+      tdRate.innerText = val.apy + " %";
+      tdEarn.innerText = "$" + useful.padDecimals(val.earnings);
+
+      // add classes
+      tr.classList.add("table-row");
+      if (index%2===0) {tr.classList.add("table-even");} else {tr.classList.add("table-odd");}
+      tdName.classList.add("table-row-item-first");
+      tdRate.classList.add("table-row-item","table-middle");
+      tdEarn.classList.add("table-row-item");
+
+
+      // first exception
+      if (index===0) {tr.id = "table-first-row";}
+
+
+      // append 
+      tr.appendChild(tdName);
+      tr.appendChild(tdRate);
+      tr.appendChild(tdEarn);
+      tableElem.appendChild(tr);
+
+    });
+
+    //last exception
+    const tr = document.createElement("tr");
+    const tdName = document.createElement("td");
+    const tdRate = document.createElement("td");
+    const tdEarn = document.createElement("td");
+    
+    tr.classList.add("table-row");
+    tdEarn.classList.add("table-note");
+
+    tdEarn.innerText = "*Based on $50,000 deposit.";
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdRate);
+    tr.appendChild(tdEarn);
+    tableElem.appendChild(tr);
+    
+
+  }
+}
+
+var data = {
+  read: function () {
+    var request = new XMLHttpRequest();
+    request.overrideMimeType("application/json");
+    request.open('GET', './code-test.json', true);
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == "200") {
+        // .open will NOT return a value but simply returns undefined in async mode so use a callback
+        
+        // raw JSON response from server request
+        var data = request.responseText;
+        // console.log("data: ", data);
+
+        // parsed JSON response -> javascript object
+        var dataObj = JSON.parse(data)
+        // console.log("json: ", dataObj);
+
+        actions.makeTable(dataObj);
       }
     }
-    return localStorage.getItem('size');
+    request.send(null);
   }
 }
 
@@ -189,10 +194,46 @@ var useful = {
   regexEscape: function(str) {
     // function to escape special characters when not done properly
     return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+  },
+	padZero: function(num) {
+		num = parseInt(num);
+    if (num < 10) {
+			num = "0" + num;
+    }
+    return num;
+	},
+	padDecimals: function(num) {
+		num = parseInt(num);
+    if (num === Math.floor(num)) {
+			num = num + ".00" ;
+    }
+    return num;
+	},
+	time: function() {
+		// returns current time in hrs:min:sec (24-hour format)
+		
+    var date = new Date();
+    var hrs = this.padZero(date.getHours());
+    var min = this.padZero(date.getMinutes());
+    var sec = this.padZero(date.getSeconds());
+		
+		var currentTime = hrs + ":" + min + ":" + sec;
+    return currentTime;
+  },
+  objectSort: function (property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
   }
-
 }
 
 
 handler.listeners();
+data.read();
 
